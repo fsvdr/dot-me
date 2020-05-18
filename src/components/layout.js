c7c84938244303d659cf52f0bac93be52b17ece7
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-05-17 19:52:54 -0500

    Implement safe area inset

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,49 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './layout.css';
+import Helmet from 'react-helmet';
 
 const Layout = ({ children }) => {
   return (
-    <div className="container">
-      <header>
-        <button type="button" onClick={() => {}}>
-          me
-          <br />
-          nu
-        </button>
+    <>
+      <Helmet
+        meta={[
+          { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover' },
+        ]}
+      />
 
-        <span className="wordmark">fsvdr</span>
-      </header>
+      <div className="container">
+        <header>
+          <button type="button" onClick={() => {}}>
+            me
+            <br />
+            nu
+          </button>
 
-      <main>{children}</main>
+          <span className="wordmark">fsvdr</span>
+        </header>
 
-      <footer>
-        <nav>
-          <li>
-            <a href="https://twitter.com/fsvdr" title="Visit me on Twitter">
-              Twitter
-            </a>
-          </li>
-          <li>
-            <a href="https://instagram.com/fsvdr.me" title="Visit me on Instagram">
-              Instagram
-            </a>
-          </li>
-          <li>
-            <a href="https://instagram.com/fsvdr" title="Visit me on Dribbble">
-              Dribbble
-            </a>
-          </li>
-          <li>
-            <a href="http://github.com/fsvdr" title="Visit me on Github">
-              Github
-            </a>
-          </li>
-        </nav>
+        <main>{children}</main>
 
-        <span>— Own it</span>
-      </footer>
-    </div>
+        <footer>
+          <nav>
+            <li>
+              <a href="https://twitter.com/fsvdr" title="Visit me on Twitter">
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/fsvdr.me" title="Visit me on Instagram">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/fsvdr" title="Visit me on Dribbble">
+                Dribbble
+              </a>
+            </li>
+            <li>
+              <a href="http://github.com/fsvdr" title="Visit me on Github">
+                Github
+              </a>
+            </li>
+          </nav>
+
+          <span>— Own it</span>
+        </footer>
+      </div>
+    </>
   );
 };
 
