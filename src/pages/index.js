d9386aa8d74b73825dfcb1af417d36c53fa0a6eb
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-05-19 19:32:57 -0500

    Implement landing hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
+
+import styles from './index.module.css';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="fsvdr" />
+
+    <h1 className={styles.huuge}>
+      Oh hi, my name is Fernando. I’m a <span className={styles.ghosty}>front end developer</span> based in Mexico City.
+    </h1>
   </Layout>
 );
 
