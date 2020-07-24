53129d44b3af05bb32f8953966b8daff834c305f
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-07-23 20:23:38 -0500

    Implement footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,72 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import CircleText from './curve-text';
 
-export const StyledFooter = styled.footer`
+const StyledFooter = styled.footer`
   display: flex;
   flex-flow: row nowrap;
-  justify-content: space-between;
   align-items: center;
-  padding: 0 0.8rem calc(env(safe-area-inset-bottom) + 1.6rem);
-  font-weight: ${props => props.theme.fontWeight.black};
+  width: 100%;
+  padding: 1.6rem 0.8rem;
   font-size: ${props => props.theme.fontSize.sm};
+  font-weight: ${props => props.theme.fontWeight.black};
+  text-transform: uppercase;
 
-  & ul {
-    display: flex;
-    flex-flow: row nowrap;
-    list-style-type: none;
-  }
-
-  & li:not(:last-child) {
-    margin-inline-end: 1.6rem;
-  }
-
-  & p {
-    margin-block-end: 0;
-  }
-
-  & span {
-    color: ${props => props.theme.color.accent};
+  & > small {
+    margin: 0 0.8rem;
   }
 
   @media screen and (min-width: ${props => props.theme.breakpoints.portraitTablet}) {
-    position: fixed;
-    top: 0;
-    right: 0.8rem;
-    min-height: 36rem;
-    height: 100vh;
-    writing-mode: vertical-lr;
-    padding-inline-start: 1.6rem;
-    padding-inline-end: 1.6rem;
-    padding-block-end: 0.8rem;
-  }
-
-  @media screen and (min-width: ${props => props.theme.breakpoints.widthConstraint}) {
-    right: calc((100vw - ${props => props.theme.breakpoints.widthConstraint}) / 2);
+    padding-inline-start: 4.4rem;
+    padding-inline-end: 4.4rem;
   }
 `;
 
 const Footer = () => (
   <StyledFooter>
-    <nav>
-      <ul>
-        <li>
-          <a href="https://twitter.com/fsvdr">Twitter</a>
-        </li>
-        <li>
-          <a href="https://instagram.com/fsvdr.me">Instagram</a>
-        </li>
-        <li>
-          <a href="https://dribbble.com/fsvdr">Dribbble</a>
-        </li>
-        <li>
-          <a href="https://github.com/fsvdr">Github</a>
-        </li>
-      </ul>
-    </nav>
-
-    <p>
-      <span>—</span> Own it
-    </p>
+    <CircleText radius={2.2} text="Original fsvdr design" badge="2020" />
+    <small>
+      Be nice y&apos;all{' '}
+      <span role="img" aria-label="peace sign">
+        ✌
+      </span>
+    </small>
   </StyledFooter>
 );
 
