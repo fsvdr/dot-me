78bbc4d765bdfab539f96c6fe1094f08e4409ebe
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-07-28 23:28:18 -0500

    Implement custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Title from '../styles/title';
+import GhostText from '../styles/ghost-text';
+import Section from '../styles/section';
+import SiteNav from '../components/site-nav';
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ location }) => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <Section>
+      <Title size="big">
+        There ain&apos;t such thing as <GhostText>{location.pathname}</GhostText>. There could be...but no.
+      </Title>
+
+      <SiteNav showContact={false} />
+    </Section>
   </Layout>
 );
+
+NotFoundPage.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default NotFoundPage;
