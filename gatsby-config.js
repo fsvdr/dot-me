5a00635a26a848fe89e91521f57d66611cba4d20
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-08-01 20:33:09 -0500

    Add PWA capabilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,18 +19,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `FSVDR — Front end developer`,
+        short_name: `FSVDR`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
+        cache_busting_mode: 'none',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        workboxConfig: {
+          globPatterns: ['**/favicon*'],
+        },
+      },
+    },
     `gatsby-plugin-eslint`,
     `gatsby-plugin-styled-components`,
     {
