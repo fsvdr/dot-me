2520f4546b99313bb08ac15debb462c923d066f2
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-08-04 21:35:23 -0500

    Restore fonts on static folder

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,6 +12,8 @@ export const wrapRootElement = ({ element }) => {
 
       <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
+
+        <link rel="stylesheet" href="fonts/Trenda/typeface-trenda.css" />
       </Helmet>
 
       {element}
