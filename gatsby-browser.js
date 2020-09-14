97ccb6e7b09c7beb8ed5ac685315c9bc9ff78856
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-13 20:06:27 -0500

    Serve fonts from CDN

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Helmet } from 'react-helmet';
 import './src/styles/global.css';
+import './src/styles/typeface-trenda.css';
 
 // eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = ({ element }) => {
@@ -10,9 +11,6 @@ export const wrapRootElement = ({ element }) => {
     <>
       <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
-
-        <link rel="preload" href="fonts/Trenda/typeface-trenda.css" as="style" />
-        <link rel="stylesheet" href="fonts/Trenda/typeface-trenda.css" media="print" onLoad="this.media='all'" />
 
         <script async defer data-domain="fsvdr.me" src="https://stats.fsvdr.me/js/index.js" />
       </Helmet>
