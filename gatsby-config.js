31b3853fc61762e63de7bef39cb88f48ad18c53e
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-13 20:33:50 -0500

    Exclude thumbnail page from sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,11 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: ['/share-thumbnail'],
+      },
+    },
   ],
 };
